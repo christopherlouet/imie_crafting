95bbe2c6edf0267bd98eefb688c14a3730650b4f
christopherlouet <christopher.louet@gmail.com>
2015-03-06 19:12:17 +0100

    add forms doc

--- src/IMIE/CraftingBundle/Form/LegType.php
+++ src/IMIE/CraftingBundle/Form/LegType.php
@@ -1,42 +1,50 @@
 <?php
 
+/**
+ * Form of a leg.
+ *
+ */
 namespace IMIE\CraftingBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class LegType extends AbstractType
-{
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('name')
-            ->add('rarity')
-            ->add('level')
-            ->add('weight')
-        ;
-    }
-    
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'IMIE\CraftingBundle\Entity\Leg'
-        ));
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'imie_craftingbundle_leg';
-    }
+/**
+ * LegType class.
+ *
+ * @author Christopher LOUËT <christopher.louet@yahoo.com>
+ *        
+ */
+class LegType extends AbstractType {
+	
+	/**
+	 * Build leg form.
+	 *
+	 * @param FormBuilderInterface $builder        	
+	 * @param array $options        	
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder->add ( 'name' )->add ( 'rarity' )->add ( 'level' )->add ( 'weight' );
+	}
+	
+	/**
+	 * Default options of leg form.
+	 *
+	 * @param OptionsResolverInterface $resolver        	
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver->setDefaults ( array (
+				'data_class' => 'IMIE\CraftingBundle\Entity\Leg' 
+		) );
+	}
+	
+	/**
+	 * Name of leg form.
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return 'imie_craftingbundle_leg';
+	}
 }
--- src/IMIE/CraftingBundle/Form/PersoType.php
+++ src/IMIE/CraftingBundle/Form/PersoType.php
@@ -1,47 +1,50 @@
 <?php
 
+/**
+ * Form of a perso.
+ *
+ */
 namespace IMIE\CraftingBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class PersoType extends AbstractType
-{
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('name')
-            ->add('level')
-            ->add('class')
-            ->add('race')
-            ->add('sexe')
-            ->add('helmet')
-            ->add('boot')
-            ->add('leg')
-            ->add('guild')
-        ;
-    }
-    
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'IMIE\CraftingBundle\Entity\Perso'
-        ));
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'imie_craftingbundle_perso';
-    }
+/**
+ * PersoType class.
+ *
+ * @author Christopher LOUËT <christopher.louet@yahoo.com>
+ *        
+ */
+class PersoType extends AbstractType {
+	
+	/**
+	 * Build perso form.
+	 *
+	 * @param FormBuilderInterface $builder        	
+	 * @param array $options        	
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder->add ( 'name' )->add ( 'level' )->add ( 'class' )->add ( 'race' )->add ( 'sexe' )->add ( 'helmet' )->add ( 'boot' )->add ( 'leg' )->add ( 'guild' );
+	}
+	
+	/**
+	 * Default options of perso form.
+	 *
+	 * @param OptionsResolverInterface $resolver        	
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver->setDefaults ( array (
+				'data_class' => 'IMIE\CraftingBundle\Entity\Perso' 
+		) );
+	}
+	
+	/**
+	 * Name of perso form.
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return 'imie_craftingbundle_perso';
+	}
 }
